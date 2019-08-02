2c530ee90e7247334404b2b1c2f943c01543b50c
KejariwalAyush <53415956+KejariwalAyush@users.noreply.github.com>
2019-08-02 09:57:46 +0530

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -2,14 +2,10 @@ class SumSqDiff
 {
 	public static void main(String args[])
 	{
-		int i=0,sum=0,s=0;
-		for(i=0;i<101;i++)
-		{
-			s+=(i*i);
-			sum+=i;
-		}
-       System.out.println((sum*sum)-s);
-       
+		int s=0,sum=0;
+		s=100*101*201/6;
+		sum=100*101/2;
+		System.out.println((sum*sum)-s);
        
 	}
 }
