78d1259c90917fcb57ecb42a78f9d765c938e855
KejariwalAyush <53415956+KejariwalAyush@users.noreply.github.com>
2019-08-02 21:39:25 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -2,23 +2,25 @@ class prime
 {
 	public static void main(String args[])
 	{
-		int i,n=0,c=0;
-		for(i=2;n<=10000;i++)
+		int i,n=0,c=0; 
+		for(i=1;n<=10001;i++)
 		{
 		
-			for(int j=2;j<=i/2;j++)
+			for(int j=2;j<i;j++)
 			{
-				if(i%j==0)
-				c++;
-			}
-			if(c==0)
-			{
-				n++;
-			}
-			c=0;
-			
-		}
-       System.out.println(i-1);
+			    c=0;
+			    if(i%j==0)
+			    {
+					
+			        c=1;
+				break;
+				
+			    }
+                         }
+                         if(c==0)
+	                  n++;
+		 }        
+              System.out.println(i-1);
    }    
 }
     
