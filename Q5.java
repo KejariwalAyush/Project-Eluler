1ae990ff4f90688707611988d90a971bc7ed87c5
KejariwalAyush <53415956+KejariwalAyush@users.noreply.github.com>
2019-08-03 17:31:14 +0530

    Update Q5.java
    Optimised and with better logic

--- Q5.java
+++ Q5.java
@@ -1,20 +1,23 @@
 package codex;
 
-class SmallestMultiple
-{
-	public static void main(String args[])
-	{
-		int i=20;
-		while (i %  2 != 0 || i %  3 != 0 || i %  4 != 0 || i %  5 != 0 ||
-         i %  6 != 0 || i %  7 != 0 || i %  8 != 0 || i %  9 != 0 ||
-         i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 ||
-         i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
-         i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
-          {
-    i += 20;
-    }
-       System.out.println(i);
-       
-       
+public class q5 {
+	public static void main(String[] args) {
+		int s = 0;
+		byte m=0;
+		for (int n = 20; n <= 1000000000; n += 20) {
+			m = 1;
+			for (int i = 20; i >= 1; i--) {
+				if (n % i != 0) {
+					m = 0;
+					break;
+				}
+			}
+			if (m == 1) {
+				s = n;
+				break;
+			}
+		}
+		System.out.println(s);
+
 	}
 }
